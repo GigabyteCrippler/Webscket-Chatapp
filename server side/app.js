fc2e3cd53d69eb90398d12b7bd14d74c8d7b4a1c
GigabyteCrippler <jbmississo@gmail.com>
2020-08-16 20:11:17 +0000

    initial commit

---
+++ server side/app.js
@@ -0,0 +1,15 @@
+// require https://yarnpkg.com/en/package/websocket.io to run
+const WebSocket = require('ws');
+
+// websocket listening on port 3030
+const wss = new WebSocket.Server({ port: 3030 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
